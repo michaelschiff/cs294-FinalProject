0b0a815711d91930e791477e7f9de05a82cb77c6
Wei Wu <wuhuwei@gmail.com>
2013-04-22 17:14:43 -0700

    fixing the errors, but now stuck at reduce

--- src/SparseMatrixBuilder.java
+++ src/SparseMatrixBuilder.java
@@ -8,35 +8,40 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.CSVLineRecordReader;
 import org.apache.hadoop.mapreduce.lib.input.CSVTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-
 public class SparseMatrixBuilder {
 
-	public class TestMapper extends Mapper<LongWritable, List<Text>, LongWritable, List<Text>> {
-		public void map(LongWritable key, List<Text> values, Context context) throws IOException, InterruptedException {
+	public static class TestMapper extends
+			Mapper<LongWritable, List<Text>, LongWritable, List<Text>> {
+		public void map(LongWritable key, List<Text> values, Context context)
+				throws IOException, InterruptedException {
 			System.out.println("TestMapper");
 			System.out.println("key=" + key);
 			int i = 0;
+			String line = "key=" + key;
 			for (Text val : values)
-				System.out.println("key=" + key + " val[" + (i++) + "] = " + val);
-			// context.write(new LongWritable(1l), new Text("1"));
+				line += " val[" + (i++) + "] = "
+						+ val;
+			System.out.println(line);
+			context.write(key, values);
 
 		}
 	}
 
 	public static class Reduce extends
-			Reducer<Text, IntWritable, Text, IntWritable> {
+			Reducer<LongWritable, List<Text>, LongWritable, List<Text>> {
 
-		public void reduce(Text key, Iterable<IntWritable> values,
+		public void reduce(LongWritable key, Iterable<List<Text>> values,
 				Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
+			List<Text> str = new LinkedList<Text>();
+			for (List<Text> val : values) {
+				str.addAll(val);
 			}
-			context.write(key, new IntWritable(sum));
+			context.write(key, str);
 		}
 	}
 
@@ -44,9 +49,9 @@ public class SparseMatrixBuilder {
 		Configuration conf = new Configuration();
 		conf.set(CSVLineRecordReader.FORMAT_DELIMITER, "\"");
 		conf.set(CSVLineRecordReader.FORMAT_SEPARATOR, ",");
-
+		
 		Job job = new Job(conf, "matrix builder");
-
+		job.setJarByClass(SparseMatrixBuilder.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 
@@ -56,8 +61,8 @@ public class SparseMatrixBuilder {
 		job.setInputFormatClass(CSVTextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.addInputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
 		job.waitForCompletion(true);
 
