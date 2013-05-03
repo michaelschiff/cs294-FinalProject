e3de30bb5ffd3379241cc7756ff7f5ead8bc71a8
Michael Schiff <schiff.michael@gmail.com>
2013-05-02 17:20:52 -0700

    added classifier pool stuff

--- src/regression/ClassifierSciKit.py
+++ src/regression/ClassifierSciKit.py
@@ -2,138 +2,140 @@ import numpy as np
 import cPickle
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, dok_matrix
 from sklearn import metrics
 import sys
+from itertools import izip_longest
 
-def main(data_file, num_features, num_samples):
-	labels = []
-	data = []
-	ij = [[],[]]
-	with open(data_file, 'r') as f:
-		
-		for i, line in enumerate(f):
-			if i >= num_samples:
-				break
-			components = line.split(' ')
-			labels.append(int(components[0]))
-			
-			for index, value in enumerate(components[1:]):
-				if index % 2 == 0:
-					ij[0].append(i)
-					ij[1].append(int(value)-1)
-					if int(value) > num_features:
-						print int(value)
-				else:
-					data.append(int(value))
+class Classifier:
+    def __init__(self, classifier, name):
+        self.name = name
+        self.classifier = classifier
+    def train(self, x, y):
+        self.classifier = self.classifier.fit(x, y)
+    def predict(self, x):
+        ad = self.classifier.predict(x)
+        return self.classifier.predict(x)
+    def evaluate(self, x, y):
+        pred = self.predict(x)
+        precision, recall, f1_score, support = metrics.precision_recall_fscore_support(y, pred)
+        fpr, tpr, thresholds = metrics.roc_curve(y, pred)
+        auc_score = metrics.auc_score(y, pred)
+        return (precision, recall, f1_score, support, fpr, tpr, thresholds, auc_score)
 
-	print len(data), len(ij[0])
-	data_set = csr_matrix((data, ij), shape=(num_samples, num_features))
-	data_set = StandardScaler(with_mean=False).fit_transform(data_set.astype(np.double))
-	classifier = GaussianNB()
+class ClassifierPool:
+    def __init__(self, classifiers):
+        self.classifiers = classifiers
+    def train(self, x, y):
+        for classifier in self.classifiers:
+            classifier.train(x,y)
+    def predict(self, x):
+        votes = np.array([0]*len(x))
+        for classifier in self.classifiers:
+            votes = np.add(votes, classifier.predict(x))
+        predictions = np.array([0]*len(x))
+        for voteIdx in xrange(len(votes)):
+            if votes[voteIdx] > len(self.classifiers)/2.0: predictions[voteIdx] = 1
+        return predictions
+    def evaluate(self, x, y):
+        pred = self.predict(x)
+        precision, recall, f1_score, support = metrics.precision_recall_fscore_support(y, pred)
+        fpr, tpr, thresholds = metrics.roc_curve(y, pred)
+        auc_score = metrics.auc_score(y, pred)
+        self.print_metrics((precision, recall, f1_score, support, fpr, tpr, thresholds, auc_score), "Ensemble Classifier")
+    def individualEvaluate(self, x, y):
+        for classifier in self.classifiers:
+            self.print_metrics(classifier.evaluate(x, y), classifier.name)
 
-	X_train, X_test, y_train, y_test = train_test_split(data_set.toarray(), labels, test_size=0.8)
+    def print_metrics(self, (precision, recall, f1_score, support, fpr, tpr, thresholds, auc_score), name):
+        print name
+        print 'precision:', precision
+        print 'recall:', recall
+        print 'f1:', f1_score
+        print '# occurrences:', support
+        print 'fpr:', fpr
+        print 'tpr:', tpr
+        print 'thresholds:', thresholds
+        print 'auc:', auc_score
+        print "\n"
 
-	classifier.fit(X_train, y_train)
-	pred_labels = classifier.predict(X_test)
-	precision, recall, f1_score, support = metrics.precision_recall_fscore_support(y_test, pred_labels)
-	print 'Gaussian NB: '
-	print 'precision:', precision
-	print 'recall:', recall
-	print 'f1:', f1_score
-	print '# occurrences:', support
-	fpr, tpr, thresholds = metrics.roc_curve(y_test, pred_labels, 1)
-	print 'fpr:', fpr
-	print 'tpr:', tpr
-	print 'thresholds:', thresholds
-	auc_score = metrics.auc_score(y_test, pred_labels)
-	print 'auc:', auc_score
-	with open('naive_bayes.pkl', 'wb') as fid:
-		cPickle.dump(classifier, fid)
-	print ''
+def load_data(datafile, num_features):
+    def grouper(iterable, n, fillvalue=None):
+        args = [iter(iterable)] * n
+        return izip_longest(*args, fillvalue=fillvalue)  
+    
+    holdoutOffset = 0
+    trainingExamples = 0
+    holdoutExamples = 0
+    print "counting Examples"
+    with open(datafile, "r") as data:
+        for line in data:
+            if (trainingExamples + holdoutExamples + holdoutOffset)%10 == 0:
+                holdoutExamples += 1
+            else:
+                trainingExamples += 1
+    print str(trainingExamples) + " training examples"
+    print str(holdoutExamples) + " holdout examples"
+    print "building matrices"
+    tX = dok_matrix((trainingExamples, num_features))
+    tY = []
+    hX = dok_matrix((holdoutExamples, num_features))
+    hY = []
+    trainingExamples = 0
+    holdoutExamples = 0
+    with open(datafile, "r") as data:
+        for line in data:
+            splitLine = line.split()
+            label = int(splitLine[0])
+            splitLine = splitLine[1:]
+            if (trainingExamples + holdoutExamples + holdoutOffset)%10 == 0:
+                hY.append(label)
+                for (i,v) in grouper(splitLine, 2):
+                    hX[holdoutExamples, int(i)-1] = float(v)
+                holdoutExamples += 1
+            else:
+                tY.append(label)
+                for (i,v) in grouper(splitLine, 2):
+                    tX[trainingExamples, int(i)-1] = float(v)
+                trainingExamples += 1
+    return (tX.todense(), hX.todense(), np.array(tY), np.array(hY))
+
+def load_data1(data_file, num_features, num_samples):
+    sillyNumber = 0
+    labels = []
+    data = []
+    ij = [[],[]]
+    with open(data_file, 'r') as f:
+        
+        for i, line in enumerate(f):
+            if i >= num_samples:
+                break
+            components = line.split(' ')
+            labels.append(int(components[0]))
+            
+            for index, value in enumerate(components[1:]):
+                if index % 2 == 0:
+                    ij[0].append(i)
+                    ij[1].append(int(value)-sillyNumber)
+                    if int(value) > num_features:
+                        print int(value)
+                else:
+                    data.append(float(value))
+
+    print len(data), len(ij[0])
+    data_set = csr_matrix((data, ij), shape=(num_samples, num_features))
+    #data_set = StandardScaler(with_mean=False).fit_transform(data_set.astype(np.double))
+    return train_test_split(data_set.toarray(), labels, test_size=0.8)
 
 
-	classifier = MultinomialNB(alpha=0.5)
-	classifier.fit(X_train, y_train)
-	pred_labels = classifier.predict(X_test)
-	precision, recall, f1_score, support = metrics.precision_recall_fscore_support(y_test, pred_labels)
-	print 'Multinomial NB: '
-	print 'precision:', precision
-	print 'recall:', recall
-	print 'f1:', f1_score
-	print '# occurrences:', support
-	fpr, tpr, thresholds = metrics.roc_curve(y_test, pred_labels, 1)
-	print 'fpr:', fpr
-	print 'tpr:', tpr
-	print 'thresholds:', thresholds
-	auc_score = metrics.auc_score(y_test, pred_labels)
-	print 'auc:', auc_score
-	with open('multinomialNB-alpha-0.5.pkl', 'wb') as fid:
-		cPickle.dump(classifier, fid)
-	print ''
 
-	classifier = LogisticRegression()
-	classifier.fit(X_train, y_train)
-	pred_labels = classifier.predict(X_test)
-	precision, recall, f1_score, support = metrics.precision_recall_fscore_support(y_test, pred_labels)
-	print 'Logistic Regression: '
-	print 'precision:', precision
-	print 'recall:', recall
-	print 'f1:', f1_score
-	print '# occurrences:', support
-	fpr, tpr, thresholds = metrics.roc_curve(y_test, pred_labels, 1)
-	print 'fpr:', fpr
-	print 'tpr:', tpr
-	print 'thresholds:', thresholds
-	auc_score = metrics.auc_score(y_test, pred_labels)
-	print 'auc:', auc_score
-	with open('logisticregression.pkl', 'wb') as fid:
-		cPickle.dump(classifier, fid)
-	print ''
-
-	classifier = KNeighborsClassifier()
-	classifier.fit(X_train, y_train)
-	pred_labels = classifier.predict(X_test)
-	precision, recall, f1_score, support = metrics.precision_recall_fscore_support(y_test, pred_labels)
-	print 'K-Neighbors (5): '
-	print 'precision:', precision
-	print 'recall:', recall
-	print 'f1:', f1_score
-	print '# occurrences:', support
-	fpr, tpr, thresholds = metrics.roc_curve(y_test, pred_labels, 1)
-	print 'fpr:', fpr
-	print 'tpr:', tpr
-	print 'thresholds:', thresholds
-	auc_score = metrics.auc_score(y_test, pred_labels)
-	print 'auc:', auc_score
-	with open('kneighbors-5.pkl', 'wb') as fid:
-		cPickle.dump(classifier, fid)
-	print ''
-	
-	
-
-	
-	# tp = 0
-	# tn = 0
-	# fp = 0
-	# fn = 0
-	# for index, test_vector in enumerate(data[int(len(data)*0.90):]):
-	# 	prediction = classifier.pred(test_vector)
-	# 	print prediction,
-	# 	if prediction and prediction == labels[int(len(labels)*0.90) + index]:
-	# 		tp += 1
-	# 	elif prediction and prediction != labels[int(len(labels)*0.90) + index]:
-	# 		fn += 1
-	# 	elif not prediction and prediction == labels[int(len(labels)*0.90) + index]:
-	# 		tn += 1
-	# 	else:
-	# 		fp += 1
-
-	# print 'tp:', tp, 'tn', tn, 'fp', fp, 'fn', fn
-
-if __name__ == '__main__':
-	main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = load_data(sys.argv[1], int(sys.argv[2]))#, int(sys.argv[3]))
+    pool = ClassifierPool([Classifier(GaussianNB(), "GNB"), Classifier(MultinomialNB(alpha=0.5), "MNB"), Classifier(LogisticRegression(), "LR"), Classifier(KNeighborsClassifier(), "KNN")])
+    pool.train(X_train, y_train)
+    pool.evaluate(X_test, y_test)
+    pool.individualEvaluate(X_test, y_test)
